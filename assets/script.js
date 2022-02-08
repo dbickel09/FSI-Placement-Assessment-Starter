17913d9fe94f9f4f75d497fef06b9ff4da60b4c4
Dan <bickel09@yahoo.com>
2022-02-07 21:01:31 -0600

    Two hour resave....

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Daniel Bickel" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -11,17 +11,79 @@ let sugar = 0 // Sugar Sprinkle
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+
+let gbPlusBtn = document.getElementById('add-gb')
+let gbMinusBtn = document.getElementById('minus-gb')
+let gbQtyDisplay = document.getElementById('qty-gb')
+
+gbPlusBtn.addEventListener('click', function() {
+    gb++
+    gbQtyDisplay.textContent = `${gb}`
+})
+
+gbMinusBtn.addEventListener('click', function() {
+    if (gb > 0) {
+    gb--
+    gbQtyDisplay.textContent = `${gb}`
+    }
+})
+
+
+let ccPlusBtn = document.getElementById('add-cc')
+let ccMinusBtn = document.getElementById('minus-cc')
+let ccQtyDisplay = document.getElementById('qty-cc')
+
+ccPlusBtn.addEventListener('click', function() {
+    cc++
+    ccQtyDisplay.textContent = `${cc}`
+})
+
+ccMinusBtn.addEventListener('click', function() {
+    if (cc > 0) {
+    cc--
+    ccQtyDisplay.textContent = `${cc}`
+    }
+})
+
+
+let sugarPlusBtn = document.getElementById('add-sugar')
+let sugarMinusBtn = document.getElementById('minus-sugar')
+let sugarQtyDisplay = document.getElementById('qty-sugar')
+
+sugarPlusBtn.addEventListener('click', function() {
+    sugar++
+    sugarQtyDisplay.textContent = `${sugar}`
+})
+
+sugarMinusBtn.addEventListener('click', function() {
+    if (sugar > 0) {
+    sugar--
+    sugarQtyDisplay.textContent = `${sugar}`
+    }
+})
+
+
+
+
+let qtyTotal = document.getElementById('qty-total')
+qtyTotal.textContent = `${gb}`
+   
+    
+
+
+
+
+
+
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
+//gbPlusBtn.addEventListener('click', function() {
 // HINT: You can delete this console.log after you no longer need it!
 console.log('Gingerbread + button was clicked!')
-
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+
 
 // TODO: Hook up event listeners for the rest of the buttons
